800a1530187a9319357525360271cd83a709016e
Tim 'mithro' Ansell <me@mith.ro>
2020-02-23 11:33:49 -0800

    Updating data to v0.0-57-g84b3e3c
    Updated to 0.0.post57 based on 84b3e3ca0ad9535acaef201c1482342871358b08 from https://github.com/m-labs/lm32.git.
    > commit 84b3e3ca0ad9535acaef201c1482342871358b08
    > Author: Michael Walle <michael@walle.cc>
    > Date:   Fri Oct 10 18:52:34 2014 +0200
    >
    >     convert the latex documention to rst
    >
    >     This will ease editing the documentation. For now only a HTML output is
    >     generated.
    >
    >     Signed-off-by: Michael Walle <michael@walle.cc>
    >
    
    Updated using v0.0-13-gb1be429 from https://github.com/litex-hub/litex-data-auto

--- litex/data/cpu/lm32/__init__.py
+++ litex/data/cpu/lm32/__init__.py
@@ -4,10 +4,10 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/m-labs/lm32.git"
 git_hash = "84b3e3ca0ad9535acaef201c1482342871358b08"
 git_describe = "v0.0-57-g84b3e3c"
-version_str = "0.0.57"
-version_tuple = (0, 0, 57)
+version_str = "0.0.post57"
+version_tuple = (0, 0)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.57")
+    pversion = V("0.0.post57")
 except ImportError:
     pass
