0cff4f1c701509ef52765d159c1bf9103a36f3e8
Tim 'mithro' Ansell <me@mith.ro>
2020-02-23 06:34:44 -0800

    Updating data to v0.0-57-g84b3e3c
    Updated to 0.0.57 based on 84b3e3ca0ad9535acaef201c1482342871358b08 from https://github.com/m-labs/lm32.git.
    > commit 84b3e3ca0ad9535acaef201c1482342871358b08
    > Author: Michael Walle <michael@walle.cc>
    > Date:   Fri Oct 10 18:52:34 2014 +0200
    >
    >     convert the latex documention to rst
    >
    >     This will ease editing the documentation. For now only a HTML output is
    >     generated.
    >
    >     Signed-off-by: Michael Walle <michael@walle.cc>
    >
    
    Updated using v0.0-11-gde841c7 from https://github.com/litex-hub/litex-data-auto

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import os
 
 from litex.data.cpu import lm32
 
-print("Found lm32 @", lm32.version_str)
+print("Found lm32 @ version", lm32.version_str, "("+lm32.git_hash+")")
 print("Data is in", lm32.data_location)
 assert os.path.exists(lm32.data_location)
 print("It contains:\n -", end=" ")
