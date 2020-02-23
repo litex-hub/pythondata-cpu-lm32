3d0fc533ea9452febb1edb29d4c2f1c0dce33fc4
Tim 'mithro' Ansell <me@mith.ro>
2020-02-22 23:46:27 -0800

    Updating data to v0.0-57-g84b3e3c
    Updated to 0.0.57 based on 84b3e3ca0ad9535acaef201c1482342871358b08 from https://github.com/m-labs/lm32.git.
    > commit 84b3e3ca0ad9535acaef201c1482342871358b08
    > Author: Michael Walle <michael@walle.cc>
    > Date:   Fri Oct 10 18:52:34 2014 +0200
    >
    >     convert the latex documention to rst
    >
    >     This will ease editing the documentation. For now only a HTML output is
    >     generated.
    >
    >     Signed-off-by: Michael Walle <michael@walle.cc>
    >
    
    Updated using v0.0-7-g9dd69d8 from https://github.com/litex-hub/litex-data-auto

---
+++ litex/data/cpu/lm32/__init__.py
@@ -0,0 +1,8 @@
+import os.path
+__dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
+data_location = os.path.join(__dir__, "verilog")
+src = "https://github.com/m-labs/lm32.git"
+git_hash = "84b3e3ca0ad9535acaef201c1482342871358b08"
+git_describe = "v0.0-57-g84b3e3c"
+from packaging.version import Version as V
+version = V("0.0.57")
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+from litex.data.cpu.lm32 import version
+
+setuptools.setup(
+    name="litex-data-cpu-lm32",
+    version=version,
+    author="LiteX Authors",
+    author_email="litex@googlegroups.com",
+    description="Python module containing data files for using the LatticeMico32 cpu with LiteX.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/litex-hub/litex-data-cpu-lm32",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Eclipse Public License 1.0 (EPL-1.0)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
