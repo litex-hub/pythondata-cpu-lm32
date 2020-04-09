52e959e871b82b020afaff0d04013a3f30b8258b
Tim 'mithro' Ansell <me@mith.ro>
2020-04-08 21:31:08 -0700

    Updating pythondata-cpu-lm32 to 0.0.post105
    Updated data to v0.0-57-g84b3e3c based on 84b3e3ca0ad9535acaef201c1482342871358b08 from https://github.com/m-labs/lm32.git.
    > commit 84b3e3ca0ad9535acaef201c1482342871358b08
    > Author: Michael Walle <michael@walle.cc>
    > Date:   Fri Oct 10 18:52:34 2014 +0200
    >
    >     convert the latex documention to rst
    >
    >     This will ease editing the documentation. For now only a HTML output is
    >     generated.
    >
    >     Signed-off-by: Michael Walle <michael@walle.cc>
    >
    
    Updated using 0.0.post48 from https://github.com/litex-hub/litex-data-auto

--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-from litex.data.cpu.lm32 import version_str
+from pythondata_cpu_lm32 import version_str
 
 setuptools.setup(
-    name="litex-data-cpu-lm32",
+    name="pythondata-cpu-lm32",
     version=version_str,
     author="LiteX Authors",
     author_email="litex@googlegroups.com",
-    description="Python module containing data files for using the LatticeMico32 cpu with LiteX.",
+    description="""\
+Python module containing verilog files for LatticeMico32 cpu.""",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/litex-hub/litex-data-cpu-lm32",
+    url="https://github.com/litex-hub/pythondata-cpu-lm32",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Eclipse Public License 1.0 (EPL-1.0)",
@@ -22,6 +23,12 @@ setuptools.setup(
     python_requires='>=3.5',
     zip_safe=False,
     packages=setuptools.find_packages(),
-    package_data={'litex.data.cpu.lm32': ['litex/data/cpu/lm32/verilog/**']},
+    package_data={
+    	'cpu_lm32': ['cpu_lm32/verilog/**'],
+    },
     include_package_data=True,
+    project_urls={
+        "Bug Tracker": "https://github.com/litex-hub/pythondata-cpu-lm32/issues",
+        "Source Code": "https://github.com/litex-hub/pythondata-cpu-lm32",
+    },
 )
--- test.py
+++ test.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 import os
 
-from litex.data.cpu import lm32
+import pythondata_cpu_lm32
 
-print("Found lm32 @ version", lm32.version_str, "(with data", lm32.data_version_str, ")")
+print("Found lm32 @ version", pythondata_cpu_lm32.version_str, "(with data", pythondata_cpu_lm32.data_version_str, ")")
 print()
-print("Data is in", lm32.data_location)
-assert os.path.exists(lm32.data_location)
-print("Data is version", lm32.data_version_str, lm32.data_git_hash)
+print("Data is in", pythondata_cpu_lm32.data_location)
+assert os.path.exists(pythondata_cpu_lm32.data_location)
+print("Data is version", pythondata_cpu_lm32.data_version_str, pythondata_cpu_lm32.data_git_hash)
 print("-"*75)
-print(lm32.data_git_msg)
+print(pythondata_cpu_lm32.data_git_msg)
 print("-"*75)
 print()
 print("It contains:")
-for root, dirs, files in os.walk(lm32.data_location):
+for root, dirs, files in os.walk(pythondata_cpu_lm32.data_location):
     dirs.sort()
     for f in sorted(files):
-        path = os.path.relpath(os.path.join(root, f), lm32.data_location)
+        path = os.path.relpath(os.path.join(root, f), pythondata_cpu_lm32.data_location)
         print(" -", path)
