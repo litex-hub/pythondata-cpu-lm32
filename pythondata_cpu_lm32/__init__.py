da9a52c4168bb0ae1376ffda3f3b3f061e5bd4ff
LiteX Robot <robot@timvideos.us>
2022-05-26 17:23:09 +0000

    Updating pythondata-cpu-lm32 to 0.0.post197
    Updated data to v0.0-57-g84b3e3c based on 84b3e3ca0ad9535acaef201c1482342871358b08 from https://github.com/m-labs/lm32.git.
    > commit 84b3e3ca0ad9535acaef201c1482342871358b08
    > Author: Michael Walle <michael@walle.cc>
    > Date:   Fri Oct 10 18:52:34 2014 +0200
    >
    >     convert the latex documention to rst
    >
    >     This will ease editing the documentation. For now only a HTML output is
    >     generated.
    >
    >     Signed-off-by: Michael Walle <michael@walle.cc>
    >
    
    Updated using 0.0.post140 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_lm32/__init__.py
+++ pythondata_cpu_lm32/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/m-labs/lm32.git"
 
 # Module version
-version_str = "0.0.post195"
-version_tuple = (0, 0, 195)
+version_str = "0.0.post197"
+version_tuple = (0, 0, 197)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post195")
+    pversion = V("0.0.post197")
 except ImportError:
     pass
 
@@ -37,11 +37,11 @@ Date:   Fri Oct 10 18:52:34 2014 +0200
 """
 
 # Tool version info
-tool_version_str = "0.0.post138"
-tool_version_tuple = (0, 0, 138)
+tool_version_str = "0.0.post140"
+tool_version_tuple = (0, 0, 140)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post138")
+    ptool_version = V("0.0.post140")
 except ImportError:
     pass
 
